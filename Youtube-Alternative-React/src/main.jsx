fa46535d9f8ae5085a0fa307589732ce8bd13e82
Doğuhan İlter <doguhannilt@gmail.com>
2023-12-18 16:57:03 +0300

    Sidebar

--- Youtube-Alternative-React/src/main.jsx
+++ Youtube-Alternative-React/src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Header from "./components/header.jsx";
-
+import Sidebar from './components/Sidebar.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Header />
+    <Sidebar />
   </React.StrictMode>,
 )
